006cbf9d630f74fd11977b402c959c1d6e4fdcf5
William Luttebie <lutterbiewilliam@gmail.com>
2026-05-10 14:21:31 -0400

    Applied more usages and comparisons of Decisions Trees into 05_ensembles.ipynb
    Also fixed a typo I found in the README.

--- SER_Project/minilearn/classifiers/__init__.py
+++ SER_Project/minilearn/classifiers/__init__.py
@@ -6,15 +6,16 @@ Implement classifier utilities here:
 - knn.py -> `KNN`
 - naive_bayes.py -> `GaussianNaiveBayes`
 - decision_tree.py -> `DecisionTreeClassifier`
+- svm.py -> `SupportVectorMachine`
 - pipeline helpers
 
 Each utility should follow a minimal, well-documented API.
 """
 
-from .decision_tree import DecisionTreeClassifier
-from .knn import KNearestNeighbors
 from .logistic import LogisticRegression
+from .knn import KNearestNeighbors
 from .naive_bayes import GaussianNaiveBayes
+from .decision_tree import DecisionTreeClassifier
 from .svm import SupportVectorMachine
 
 __all__ = ["LogisticRegression", "DecisionTreeClassifier", "KNearestNeighbors", "GaussianNaiveBayes", "SupportVectorMachine"]
